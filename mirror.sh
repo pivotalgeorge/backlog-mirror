82d97a419d8b24e8ed6f6063ef10c7af687a1547
George Gelashvili <ggelashvili+cf@pivotal.io>
2019-05-09 19:31:37 -0400

    WIP get stories automagically

--- mirror.sh
+++ mirror.sh
@@ -23,10 +23,21 @@ fi
 CSV_IN_FILE="$HOME/Downloads/in.csv"
 CSV_OUT_FILE="out.csv"
 
+test_endpoint_errors=$(curl -Ss -X GET -H "X-TrackerToken: $TRACKER_API_TOKEN" https://www.pivotaltracker.com/services/v5/me | jq .code)
+if [ $test_endpoint_errors != "null" ]; then
+  echo $test_endpoint_errors
+  echo 'Auth check failed. is $TRACKER_API_TOKEN valid?'
+fi
+
 private_backlog_manual_url="https://www.pivotaltracker.com/projects/$PRIVATE_BACKLOG_ID/export"
 echo
 echo "Dear user: please download csv from $private_backlog_manual_url to ~/Downloads/in.csv. [Return] to continue"
 read
+#curl -X POST -H "X-TrackerToken: $TRACKER_API_TOKEN" -H "Content-Type: application/json" -d '{"story_ids":[551]}' "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/export"
+#PUBLIC_STORIES=$(
+PROJECT_ID=$PRIVATE_PROJECT_ID
+curl -X GET -H "X-TrackerToken: $TRACKER_API_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories" #|jq '.[] | select ( .labels | .[] | select (.name=="public"))'
+
 
 head -1 $CSV_IN_FILE > $CSV_OUT_FILE
 cat $CSV_IN_FILE | awk -F ',' '{label=$3; if (label=="public") print $0}' >> $CSV_OUT_FILE
