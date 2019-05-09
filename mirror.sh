67c0401ccf1d2119010f4a33f0f98b5dfd998613
George Gelashvili <ggelashvili+cf@pivotal.io>
2019-05-09 18:41:06 -0400

    Improve csv location paths and fix label filter (awk)

--- mirror.sh
+++ mirror.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
-set -ex
+set -e
 
 # environment:
 #TRACKER_API_TOKEN
 
-CSV_IN_FILE="in.csv"
+CSV_IN_FILE="$HOME/Downloads/in.csv"
 CSV_OUT_FILE="out.csv"
 
 PUBLIC_BACKLOG_URL="https://www.pivotaltracker.com/n/projects/2345567"
 echo
-echo "Dear user, please download csv from $PRIVATE_BACKLOG_URL"
+echo "Dear user, please download csv from $PRIVATE_BACKLOG_URL to ~/Downloads/in.csv"
 echo
+read
 
 head -1 $CSV_IN_FILE > $CSV_OUT_FILE
-cat $CSV_IN_FILE | awk -F ',' '{label=$3; entire_line=$0 if (label=="public") print entire_line}' >> $CSV_OUT_FILE
+cat $CSV_IN_FILE | awk -F ',' '{label=$3; if (label=="public") print $0}' >> $CSV_OUT_FILE
 
 PUBLIC_BACKLOG_URL="https://www.pivotaltracker.com/n/projects/2345570"
 echo
 echo "Dear user, please delete all stories in $PUBLIC_BACKLOG_URL"
 echo
+read
 
 # DELETE_URL="https://www.pivotaltracker.com/services/v5/commands?envelope=true"
 # # TODO idk maybe use a non-internal API endpoint or something
 # curl -X POST $DELETE_URL
 
 echo
-echo "Dear user, please upload csv to $PUBLIC_BACKLOG_URL"
+echo "Dear user, please upload out.csv to $PUBLIC_BACKLOG_URL"
 echo
 
 
