b6be628f7f81548145d242ef3186bf67e1252d98
George Gelashvili <ggelashvili+cf@pivotal.io>
2019-05-15 18:32:52 -0400

    WIPPY almost at POSTing filtered stories to public backlog
    Co-authored-by: Jackson Feeny <jfeeny@pivotal.io>

--- mirror.sh
+++ mirror.sh
@@ -32,11 +32,13 @@ fi
 private_backlog_manual_url="https://www.pivotaltracker.com/projects/$PRIVATE_BACKLOG_ID/export"
 echo
 echo "Dear user: please download csv from $private_backlog_manual_url to ~/Downloads/in.csv. [Return] to continue"
-read
 #curl -X POST -H "X-TrackerToken: $TRACKER_API_TOKEN" -H "Content-Type: application/json" -d '{"story_ids":[551]}' "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/export"
 #PUBLIC_STORIES=$(
 PROJECT_ID=$PRIVATE_PROJECT_ID
-# curl -X GET -H "X-TrackerToken: $TRACKER_API_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PRIVATE_BACKLOG_ID/stories" #|jq '.[] | select ( .labels | .[] | select (.name=="public"))'
+VAR0=$(curl -X GET -H "X-TrackerToken: $TRACKER_API_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PRIVATE_BACKLOG_ID/stories")
+VAR1=$(curl -X GET -H "X-TrackerToken: $TRACKER_API_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PRIVATE_BACKLOG_ID/stories" |jq -c '.[] | select ( .labels | .[] | select (.name=="public")) | .name')
+echo $VAR0
+read
 
 
 head -1 $CSV_IN_FILE > $CSV_OUT_FILE
@@ -45,14 +47,12 @@ cat $CSV_IN_FILE | awk -F ',' '{label=$3; if (label=="public") print $0}' >> $CS
 #PUBLIC_BACKLOG_URL="https://www.pivotaltracker.com/n/projects/2345570"
 public_backlog_url="https://www.pivotaltracker.com/n/projects/$PUBLIC_BACKLOG_ID"
 echo "Dear user: please DELETE all stories in $public_backlog_url. [Return] to continue"
-read
-#VAR=$(curl -X GET -H "X-TrackerToken: $TRACKER_API_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PUBLIC_BACKLOG_ID/stories" |jq '.[] | select ( .labels | .[] | select (.name=="public"))')
 VAR2=$(curl -X GET -H "X-TrackerToken: $TRACKER_API_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PUBLIC_BACKLOG_ID/stories" |jq '.[] | select ( .labels | .[] | select (.name=="public")) |.id')
 echo $VAR2
 for s in $VAR2; do
   curl -X DELETE -H "X-TrackerToken: $TRACKER_API_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PUBLIC_BACKLOG_ID/stories/$s"
 done
-#curl -X POST -H "X-TrackerToken: $TRACKER_API_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PUBLIC_BACKLOG_ID/stories" |jq '.[] | select ( .labels | .[] | select (.name=="public"))'
+read
 
 # DELETE_URL="https://www.pivotaltracker.com/services/v5/commands?envelope=true"
 # # TODO idk maybe use a non-internal API endpoint or something
@@ -60,5 +60,14 @@ done
 
 public_backlog_manual_url="https://www.pivotaltracker.com/projects/$PUBLIC_BACKLOG_ID/import"
 echo "Dear user: please upload $(dirname $0)/out.csv to $public_backlog_manual_url"
-echo
+
+# echo $VAR1 | jq -c '.[]' | while read s; do
+echo "$VAR1" | while IFS= read -r line ; do
+# for s in $VAR1; do
+  payload="{\"name\": $line}"
+  echo "I don't do sadness"
+  echo \'$payload\'
+  curl -X POST -H "X-TrackerToken: $TRACKER_API_TOKEN" -H "Content-Type: application/json" -d \'${payload}\' "https://www.pivotaltracker.com/services/v5/projects/$PUBLIC_BACKLOG_ID/stories"
+  # curl -X POST -H "X-TrackerToken: $TRACKER_API_TOKEN" -d '{"name": "Buggy chore"}' "https://www.pivotaltracker.com/services/v5/projects/$PUBLIC_BACKLOG_ID/stories"
+done
 
